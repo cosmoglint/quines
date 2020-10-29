069e829d0f2d446187edf03be75ed4dd4d55f47d
thedoodler <ramprakashsmail@gmail.com>
2020-10-29 17:18:43 +0530

    learnt about python quines

--- js_quine.js
+++ js_quine.js
@@ -1,1 +1,7 @@
 s="s=%j;console.log(s,s)";console.log(s,s)
+
+
+// (function a(){console.log('('+a+')()')})()
+
+
+code='var q=unescape("%27");console.log("code="+q+code+q+";eval(code)")';eval(code)
---
+++ python_quine.py
@@ -0,0 +1,6 @@
+s = 's = %r;print(s%%s)';print(s%s)
+
+
+# I, did not think semicolons were allowed in python.
+# also %r is useful for printing with the quotation marks
+# %%  means after substitution you get a single %
